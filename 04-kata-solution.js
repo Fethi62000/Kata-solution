5603b0cf0d8f9b927bd2e615ddaf53b162d0cda8
Fethisensei <fetam62@gmail.com>
2024-05-21 07:00:24 +0200

    docs(04-05-06-07-08-kata-solution):add file

---
+++ 04-kata-solution.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+# Minimum
+
+Write a function min that takes two arguments and returns their minimum.
+
+
+```
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+```
+*/
+
